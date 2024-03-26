3e0be68a2645842034fcab69d99ef2a7ce006023
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-03-26 21:25:36 +0500

    Add files via upload

---
+++ Day 22/question64.ts
@@ -0,0 +1,5 @@
+function numString(string: string, number: number){
+    console.log(string + number );
+}
+
+numString("Age: ",20);
---
+++ Day 22/question66.ts
@@ -0,0 +1,5 @@
+function checkBothTrue(boolean1: boolean, boolean2: boolean): boolean{
+    return boolean1 && boolean2
+}
+
+console.log(checkBothTrue(true,false))
