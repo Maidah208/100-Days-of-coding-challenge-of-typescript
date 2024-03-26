b91b78b358e48962aeeea9453a23b4cbbcbb60ba
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-03-26 22:15:03 +0500

    Add files via upload

---
+++ Day 24/question71.ts
@@ -0,0 +1,13 @@
+let age = 20;
+console.log(age);
+
+age = 19;
+console.log(age);
+
+const myName = "Karen";
+console.log(myName);
+try {
+    myName = "Daly"
+} catch (error){
+    console.log(error)
+}
---
+++ Day 24/question72.ts
@@ -0,0 +1,9 @@
+{
+    let age = 19;
+    const myName = "Karen";
+    console.log(age); //output: 19
+    console.log(myName); //output: Karen
+}
+
+console.log(age) // This line will throw an error.
+console.log(myName) // This line will also throw an error.
